5eff5280f5c31bf070a3b06a73c9f8eeb0b81c4d
malumunayco <malumunaycoa05@gmail.com>
2026-05-13 18:33:17 -0500

    feat(stats): add statistics section with health metrics cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar';
 import { HeroComponent } from './components/hero/hero';
+import { StatsComponent } from './components/stats/stats';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent, HeroComponent],
+  imports: [
+    NavbarComponent,
+    HeroComponent,
+    StatsComponent
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
