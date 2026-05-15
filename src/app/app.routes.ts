9bdebfaffb97c4fd15dba460fa53ca6f9e935332
malumunayco <malumunaycoa05@gmail.com>
2026-05-15 01:33:13 -0500

    feat(terms): add terms of service page with usage and billing details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PrivacyComponent } from './pages/privacy/privacy';
 import { HomeComponent } from './pages/home/home';
+import { TermsComponent } from './pages/terms/terms';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'privacy', component: PrivacyComponent }
+    { path: '', component: HomeComponent },
+    { path: 'privacy', component: PrivacyComponent },
+    { path: 'terms', component: TermsComponent }
 ];
